96d5a9a35a7ee5c19d562add6d98a819a9de8a5e
jblang94 <jbarlang@gmail.com>
2016-07-27 07:24:06 +0000

    implemented show and delete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
-require 'faker'
+Contact.create!(name: "Jackie Lang", email: "jbarlang@gmail.com")
+Contact.create!(name: "Steve Lang", email: "sflang@gmail.com")
+Contact.create!(name: "Helena Lang", email: "hbarlang@gmail.com")
 
-dad = Contact.create!(name: "Steve Lang", email: "sflang@gmail.com")
-mom = Contact.create!(name: "Helena Lang", email: "hbarlang@gmail.com")
-
-10.times do 
-  name = Faker::Name.name
-  email = Faker::Internet.email
-  Contact.create!(name: name, email: email)
-end
+Contact.create!(name: "Toni Bardos", email: "mabardos@aol.com")
+Contact.create!(name: "Donald Trump", email: "dummy@thedumbest.com")
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,21 +1,54 @@
 $(function() {
+
+  function attachOptions(contactEntry) {
+    var options = $('<td></td>');
+    var showBtn = $('<button></button>').text('Show').addClass('button').addClass('show');
+    var deleteLink = $('<a></a>').attr('href', '/contacts/' + contactEntry.data('id') + '/delete').text('Delete').addClass('button').addClass('delete');
+    showBtn.appendTo(options);
+    deleteLink.appendTo(options);
+    options.appendTo(contactEntry);
+  }
+
   $('.add-contact').on('submit', function(event) {
     event.preventDefault();
-    addContactForm = $(this);
-
+    var addContactForm = $(this);
     $.ajax({
       method: addContactForm.attr('method'),
       url: addContactForm.attr('action'),
       data: addContactForm.serialize(),
       success: function(data) {
         contactEntry = $('<tr></tr>').addClass('contact');
-        contactEntry.attr('data-id', data.id);
-        contactEntry.attr('data-name', data.name);
-        contactEntry.attr('data-email', data.email);
-        $('<td></td>').text(data.id).appendTo(contactEntry);
+        contactEntry.attr('data-id', data.id).attr('data-name', data.name).attr('data-email', data.email);
         $('<td></td>').text(data.name).appendTo(contactEntry);
-        $('<td></td>').text(data.email).appendTo(contactEntry);
-        contactEntry.appendTo('tbody');
+        attachOptions(contactEntry);
+        contactEntry.appendTo('.contacts-table');
+        $('.new-contact-name').val("");
+        $('.new-contact-email').val(""); 
+      }
+    });
+  });
+
+  $('.contacts-table').on('click', '.show', function() {
+    showContact = $('.show-contact');
+    showContact.empty();
+    contact = $(this).closest('.contact');
+
+    $('<p></p>').text("CONTACT ID: " + contact.data('id')).appendTo(showContact);
+    $('<p></p>').text("CONTACT NAME: " + contact.data('name')).appendTo(showContact);
+    $('<p></p>').text("CONTACT EMAIL: " + contact.data('email')).appendTo(showContact);
+  });
+
+  $('.contacts-table').on('click', '.delete', function(event) {
+    event.preventDefault();
+    var contact = $(this).closest('.contact');
+    var contactId = contact.data('id');
+    var deleteUrl = '/contacts/' + contactId + '/delete';
+    $.ajax({
+      method: 'GET',
+      url: deleteUrl,
+      success: function() {
+        contact.remove();
+        alert("Deleted contact " + contactId);
       }
     });
   });
