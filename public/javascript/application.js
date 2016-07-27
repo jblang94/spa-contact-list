fffd65bbbe328431553476045cf6ef8a1accac77
jblang94 <jbarlang@gmail.com>
2016-07-27 03:58:05 +0000

    added data fields to <tr>

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -9,6 +9,9 @@ $(function() {
       data: addContactForm.serialize(),
       success: function(data) {
         contactEntry = $('<tr></tr>').addClass('contact');
+        contactEntry.attr('data-id', data.id);
+        contactEntry.attr('data-name', data.name);
+        contactEntry.attr('data-email', data.email);
         $('<td></td>').text(data.id).appendTo(contactEntry);
         $('<td></td>').text(data.name).appendTo(contactEntry);
         $('<td></td>').text(data.email).appendTo(contactEntry);
